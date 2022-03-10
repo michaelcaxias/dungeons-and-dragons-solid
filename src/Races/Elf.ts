efcc8225393eb016830cd54cf8174f9c1b714d76
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-10 17:07:16 -0300

    feat: adiciona método createdRacesInstances em cada classe // incrementa o numero de instancias em cada classe

--- src/Races/Elf.ts
+++ src/Races/Elf.ts
@@ -4,9 +4,18 @@ export default class Elf extends Race {
   maxLifePoints = 99;
   name: string;
   dexterity: number;
+  static numberOfInstances = 0;
+
   constructor(name: string, dextery: number) {
     super(name, dextery);
     this.name = name;
     this.dexterity = dextery;
+    Elf.numberOfInstances += 1;
+  }
+
+  static createdRacesInstances():number {
+    // a resposta da kariane.Langworth54 me ajudou
+    // https://helperbyte.com/questions/461240/how-to-create-a-counter-class-to-count-how-many-times-it-is-called
+    return this.numberOfInstances;
   }
 }
--- src/Races/Orc.ts
+++ src/Races/Orc.ts
@@ -4,9 +4,18 @@ export default class Orc extends Race {
   maxLifePoints = 74;
   name: string;
   dexterity: number;
+  static numberOfInstances = 0;
+
   constructor(name: string, dextery: number) {
     super(name, dextery);
     this.name = name;
     this.dexterity = dextery;
+    Orc.numberOfInstances += 1;
+  }
+
+  static createdRacesInstances():number {
+    // a resposta da kariane.Langworth54 me ajudou
+    // https://helperbyte.com/questions/461240/how-to-create-a-counter-class-to-count-how-many-times-it-is-called
+    return this.numberOfInstances;
   }
 }
