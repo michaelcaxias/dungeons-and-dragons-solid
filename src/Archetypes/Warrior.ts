065d1ca8a4bd5f943891abed5e2b11e97b17eb41
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-10 18:36:02 -0300

    feat: ajusta nome de Warrior

--- src/Archetypes/Warrior.ts
+++ src/Archetypes/Warrior.ts
@@ -1,14 +1,14 @@
 import { EnergyType } from '../Energy';
 import Archetype from './Archetype';
 
-export default class Ranger extends Archetype {
+export default class Warrior extends Archetype {
   static numberOfInstances = 0;
   readonly energy = 'stamina';
   readonly name: string;
   constructor(name: string) {
     super(name);
     this.name = name;
-    Ranger.numberOfInstances += 1;
+    Warrior.numberOfInstances += 1;
   }
   
   static createdArchetypeInstances(): number {
