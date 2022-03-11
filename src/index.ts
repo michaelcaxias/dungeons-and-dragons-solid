694bdb6c7c81611a11ef926eedc96a8dce40b187
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-11 19:13:30 -0300

    feat: cria função runBattles

--- src/index.ts
+++ src/index.ts
@@ -3,25 +3,28 @@ import Monster from './Monster';
 import Dragon from './Dragon';
 import PVP from './Battle/PVP';
 import PVE from './Battle/PVE';
+import Battle from './Battle/Battle';
 
 const player1 = new Character('');
+
 player1.levelUp();
 player1.levelUp();
 player1.levelUp();
+
 const player2 = new Character('');
 const player3 = new Character('');
-
-export { player1, player2, player3 };
 
 const monster1 = new Monster();
 const monster2 = new Dragon();
 
-export { monster1, monster2 };
-
 const pvp = new PVP(player2, player3);
-
-export { pvp };
 
 const pve = new PVE(player1, [monster1, monster2]);
 
-export { pve };
+const runBattles = (battles: Battle[]) => {
+  battles.forEach((battle) => {
+    battle.fight();
+  });
+};
+
+export { runBattles, pve, pvp, monster1, monster2, player1, player2, player3 };
