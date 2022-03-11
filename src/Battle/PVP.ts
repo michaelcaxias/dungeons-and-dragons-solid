3407c4f618f838b700796ebde6da93ae639cfcaa
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-11 17:32:23 -0300

    feat: implementa função de batalha

--- src/Battle/PVP.ts
+++ src/Battle/PVP.ts
@@ -12,4 +12,15 @@ export default class PVP extends Battle {
     this.firstCharacter = firstCharacter;
     this.secondCharacter = secondCharacter;
   }
+
+  fight(): number {
+    const player1LifePoints = this.firstCharacter.lifePoints;
+    const player2LifePoints = this.secondCharacter.lifePoints;
+    // if player 1 wins, returns 1
+    if (player1LifePoints > player2LifePoints) return 1;
+    // if player 2 wins, returns -1
+    if (player2LifePoints > player1LifePoints) return -1;
+    // otherwise returns 0
+    return 0;
+  }
 }
