4ea71486270854fe5b19e5fedc00856770824ed1
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-11 17:14:36 -0300

    feat: importa SimpleFighter em Character para solucionar problema em Monster

--- src/Character.ts
+++ src/Character.ts
@@ -1,4 +1,4 @@
-import Fighter from './Fighter';
+import Fighter, { SimpleFighter } from './Fighter';
 import Race, { Elf } from './Races';
 import Archetype, { Mage } from './Archetypes';
 import Energy from './Energy';
@@ -63,7 +63,7 @@ export default class Character implements Fighter {
     return this._lifePoints;
   }
 
-  attack(enemy: Fighter): void {
+  attack(enemy: Fighter | SimpleFighter): void {
     enemy.receiveDamage(this._strength);
   }
 
@@ -83,4 +83,4 @@ export default class Character implements Fighter {
   special(enemy: Fighter): void {
     console.log(`${this._maxLifePoints} ${enemy}`);
   }
-} 
+}
