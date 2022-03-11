d827ebb8c20739357ac2aca360df3db49124a8cc
Michael Caxias <michaelcaxias.dev@gmail.com>
2022-03-11 16:11:39 -0300

    feat: levelUp passando

--- src/Character.ts
+++ src/Character.ts
@@ -67,17 +67,17 @@ export default class Character implements Fighter {
     enemy.receiveDamage(this._strength);
   }
 
-  levelUp(): void {
+  levelUp(): number {
     this._maxLifePoints += getRandomInt(1, 10);
     this._strength += getRandomInt(1, 10);
     this._dexterity += getRandomInt(1, 10);
     this._defense += getRandomInt(1, 10);
     this._energy.amount = 10;
-    if (this._maxLifePoints < this.race.maxLifePoints) {
-      this._maxLifePoints = this._lifePoints;
-    } else {
+    if (this._maxLifePoints > this.race.maxLifePoints) {
       this._maxLifePoints = this.race.maxLifePoints;
     }
+    this._lifePoints = this._maxLifePoints;
+    return this._lifePoints;
   }
 
   special(enemy: Fighter): void {
